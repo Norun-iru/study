165ff20586ab0fe5f98d1fa96a9a8a88d2d198da
のるん <97i.issei@gmail.com>
2021-07-17 11:34:25 +0900

    うお

--- Unit8/8-3.c
+++ Unit8/8-3.c
@@ -2,10 +2,12 @@
 int main(void){
   int i;
   int s = 0;
+  printf("正の整数を入力してください：");
   scanf("%d", &i);
+  int j = i;
   for(i = i; i > 0; i--){
     s = s + i;
   }
-    printf("%d", s);
+  printf("1 から %d までをを足した値は %d です。", j,s);
   return 0;
 }
